77ca7ddadb14a3a6398f1c8a0f999cb737882111
Abhijeet Nagarkar <abhijeet.nagarkar@springernature.com>
2019-05-07 09:40:43 +0530

    updated

--- TestScripts/Demo/src/test/java/Demolink/Demo/NewTest.java
+++ TestScripts/Demo/src/test/java/Demolink/Demo/NewTest.java
@@ -3,22 +3,46 @@ package Demolink.Demo;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 
 public class NewTest {
 	WebDriver driver;
+	@FindBy(xpath = "//*[@id=\"tsf\"]/div[2]/div/div[1]/div/div[1]/input")
+	  WebElement fl;
 	public String test="Hi";
 	
   @Test
   public void f() {
   }
+  /*@BeforeMethod
+  public void beforeClass()
+  {
+  	 
+  }
+  /*
+  static NewTest init(WebDriver webDriver) {
+      return new NewTest(webDriver);
+  }
+  
+  */
+  
   @BeforeTest
   public void beforeTest() {
 	  System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+	  driver=new ChromeDriver();
+	  PageFactory.initElements(driver, this);
+	
+	//  PageFactory.initElements(driver, this);
+	
 	  
-	   driver=new ChromeDriver();
 	   driver.get("https://www.google.com");
+	   fl.sendKeys("selenium tutorial");
+	   
   }
 
   @AfterTest
